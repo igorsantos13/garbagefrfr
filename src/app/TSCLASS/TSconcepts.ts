471b06d5fb1301dad015f6228609468b10d7a848
Igor Santos <igorsantos9513@outlook.com>
2023-06-22 18:29:32 -0300

    TS concepts part2

--- src/app/TSCLASS/TSconcepts.ts
+++ src/app/TSCLASS/TSconcepts.ts
@@ -33,8 +33,11 @@ function mockFunction<T>(args: T): T{
 }
 
 //PICK - one property from a type
-type idProps = Pick<PersonProps, 'personID'>; //has only the personID property
-type emailProp = Pick<ContactProps, 'email'>; //has the email only
+
+//set a new attribute + PICK's the personId from Person props
+type IdProps = {CNH: 'string'} & Pick<PersonProps, 'personID'>;
+
+type EmailProp = Pick<ContactProps, 'email'>; //has the email only
 
 //OMIT - choose an property to be removed
 type AddressWithoutZipCode = Omit<AddressProps, 'zipCode'>
@@ -43,3 +46,52 @@ type AddressWithoutZipCode = Omit<AddressProps, 'zipCode'>
 type PersonWithoutAge = Omit<PersonProps, 'age'>
 //same as the prior example, here we take all properties (name, personID)
 //but we exclude the 'age' one.
+
+//type guards
+function square(a: number | string){
+    if(typeof a === 'number'){
+        return `Result: ${a * a}`
+    }
+
+    throw new Error(
+        `Error - it was not possible to calculate due to type: ${typeof a}`
+    )
+}
+
+//Record
+type TeamNameProps = {
+    name: 'string',
+    nickname: 'string',
+    acronym: 'string',
+}
+
+//Instead of wasting time setting the same type for all properties 
+//you can use Record to set them all at once
+
+type JerseyProps = Record<'color' |'size' | 'model', string>
+//Record demands two arguments,
+//First add all properties,
+//Then, as second argumetn you add the type.
+
+//Also we are able to do the opposite
+
+type Months =
+    | 'Janeiro'
+    | 'Fevereiro'
+    | 'Março'
+    | 'Abril'
+    | 'Maio'
+    | 'Junho'
+    | 'Julho'
+    | 'Agosto'
+    | 'Setembro'
+    | 'Outubro'
+    | 'Novembro'
+    | 'Dezembro'
+
+type AllMonthsVasco = Record<Months, 'vasco'>
+//now all properties has been set to the value 'vasco'
+
+
+//Credits: Dev Junior Alves.
+//Source: https://www.youtube.com/watch?v=7DhlW2HvztE
